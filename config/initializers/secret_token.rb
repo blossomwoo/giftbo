470bc2b0f8ccde24a591a613862437ce88688a72
capripot <capripot@gmail.com>
2013-06-22 13:11:05 -0400

    first version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Giftbo::Application.config.secret_token = '8b229b76020688baaa8c388995bfb99f21dc8b67889a99e81452f06804b27043210a2390e2c11c097d34f518e07d4983108388d5fd5915acc2484fb16900d60a'
